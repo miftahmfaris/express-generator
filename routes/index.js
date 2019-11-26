35e4ce018d3da4f3205cf2033e2e8390f1e310df
miftah faris <himeprojects@gmail.com>
2019-11-26 14:43:05 +0700

    add new route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/home", (req, res) => {
+  res.json({ message: "ini adalah route home" });
 });
 
 module.exports = router;
